5ef1baeb995aa8008c16d188ed4d8a53f7d5296b
katianavarez <katia.navarez252855@potros.itson.edu.mx>
2025-05-30 05:11:17 -0700

    +doc

--- AgenciaFiscalPresentacion/src/main/java/control/Control.java
+++ AgenciaFiscalPresentacion/src/main/java/control/Control.java
@@ -18,16 +18,12 @@ import dtos.LicenciaDTO;
 import dtos.PersonaDTO;
 import dtos.PlacaDTO;
 import excepciones.NegocioException;
-import idaos.ILicenciaDAO;
-import idaos.IPersonaDAO;
 import interfaces.IAutomovilBO;
 import interfaces.ILicenciaBO;
 import interfaces.IPersonaBO;
 import interfaces.IPlacaBO;
 import interfaces.IVehiculoBO;
 import java.util.List;
-import mappers.LicenciaMapper;
-import mappers.PersonaMapper;
 import pantallas.PantallaConsultarHistorialPlacas;
 import pantallas.PantallaInicio;
 import pantallas.PantallaMenuPlacas;
@@ -36,7 +32,10 @@ import pantallas.PantallaPlacasAutoUsado;
 import pantallas.PantallaRegistroPersonaYLicencia;
 
 /**
- *
+ * Clase que controla el flujo de navegación entre pantallas y 
+ * delega la lógica a la capa de negocio.
+ * Implementa singleton.
+ * 
  * @author katia
  */
 public class Control {
@@ -60,6 +59,11 @@ public class Control {
         inicializarPantallas();
     }
     
+    /**
+     * Obtiene la única instancia del controlador.
+     * 
+     * @return instancia única del controlador.
+     */
     public static Control getInstancia() {
         if (instancia == null) {
             instancia = new Control();
@@ -67,6 +71,9 @@ public class Control {
         return instancia;
     }
     
+    /**
+     * Inicializa todas las instancias de los objetos de negocio.
+     */
     private void inicializarBOs() {
         this.licenciaBO = new LicenciaBO(new LicenciaDAO());
         this.personaBO = new PersonaBO(new PersonaDAO(), new LicenciaDAO(), this.licenciaBO);
@@ -75,6 +82,9 @@ public class Control {
         this.automovilBO = new AutomovilBO(new AutomovilDAO());
     }
     
+    /**
+     * Inicializa todas las pantallas de la aplicación.
+     */
     private void inicializarPantallas() {
         this.pantallaInicio = new PantallaInicio();
         this.pantallaRegistro = new PantallaRegistroPersonaYLicencia();
@@ -134,6 +144,9 @@ public class Control {
         pantallaConsultarHistorialPlacas.setVisible(true);
     }
     
+    /**
+     * Oculta todas las pantallas de la aplicación.
+     */
     private void ocultarTodas() {
         pantallaInicio.setVisible(false);
         pantallaRegistro.setVisible(false);
@@ -143,26 +156,64 @@ public class Control {
         pantallaConsultarHistorialPlacas.setVisible(false);
     }
     
+    /**
+     * Inicia la aplicación mostrando la pantalla de inicio.
+     */
     public void iniciar() {
         mostrarPantallaInicio();
     }
     
+    /**
+     * Registra una persona junto con su licencia.
+     * 
+     * @param personaDTO DTO con los datos de la persona.
+     * @param licenciaDTO DTO con los datos de la licencia.
+     * @throws NegocioException si ocurre un error en la lógica de negocio.
+     */
     public void registrarPersonaYLicencia(PersonaDTO personaDTO, LicenciaDTO licenciaDTO) throws NegocioException {
         personaBO.registrarPersonaConLicencia(personaDTO, licenciaDTO);
     }
     
+    /**
+     * Genera los datos para una placa de auto nuevo.
+     * 
+     * @return DTO con los datos generados.
+     */
     public PlacaDTO generarDatosPlacaAutoNuevo() {
         return placaBO.generarPlacaAutoNuevo();
     }
     
+    /**
+     * Registra una placa para un auto nuevo.
+     * 
+     * @param dto DTO con los datos de la placa.
+     * @param rfc RFC de la persona.
+     * @return DTO de la placa registrada.
+     * @throws NegocioException si ocurre un error en la lógica de negocio.
+     */
     public PlacaDTO registrarPlacaAutoNuevo(PlacaDTO dto, String rfc) throws NegocioException {
         return placaBO.registrarPlacaAutoNuevo(dto, rfc);
     }
     
+    /**
+     * Registra una placa para un auto usado.
+     * 
+     * @param dto DTO con los datos de la placa.
+     * @param rfcPersona RFC de la persona.
+     * @return DTO de la placa registrada.
+     * @throws NegocioException si ocurre un error en la lógica de negocio.
+     */
     public PlacaDTO registrarPlacaAutoUsado(PlacaDTO dto, String rfcPersona) throws NegocioException {
         return placaBO.registrarPlacaAutoUsado(dto, rfcPersona);
     }
     
+    /**
+     * Obtiene el historial de placas asociadas a un número de serie de vehículo.
+     * 
+     * @param numeroSerie Número de serie del vehículo.
+     * @return Lista de DTOs con el historial de placas.
+     * @throws NegocioException si ocurre un error en la lógica de negocio.
+     */
     public List<PlacaDTO> obtenerHistorialPlacas(String numeroSerie) throws NegocioException {
         return placaBO.obtenerHistorialPorNumSerie(numeroSerie);
     }
